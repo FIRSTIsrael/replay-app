ae5c37db432ca0e353b48d4fe654c047a02596bf
Idan Stark <idanstark42@gmail.com>
2021-01-01 15:01:30 +0200

    Added option for uploading video

--- logic/video_processing.js
+++ logic/video_processing.js
@@ -1,8 +1,27 @@
 import * as FileSystem from 'expo-file-system'
 import shortid from 'shortid'
+import { AsyncStorage } from 'react-native'
 
-export const processVideo = async video => {
+const copyVideo = async video => {
   const videoId = shortid.generate()
   await FileSystem.makeDirectoryAsync(`${FileSystem.documentDirectory}videos/`, { intermediates: true })
-  await FileSystem.moveAsync({ from: video.uri, to: `${FileSystem.documentDirectory}videos/demo_${videoId}.mov` })
+  return await FileSystem.moveAsync({ from: video.uri, to: `${FileSystem.documentDirectory}videos/demo_${videoId}.mov` })
+}
+
+const uploadVideo = async video => {
+  const method = 'post'
+  const body = new FormData();
+
+  const name = 'video-upload'
+  const type = `video/${video.codec || 'mp4'}`
+  const ui = video.uri
+
+  body.append("video", { name, type, uri })
+
+  const teamVideoUrl = await AsyncStorage.getItem('teamVideoUrl')
+  return await fetch(teamVideoUrl, { method, body })
+}
+
+export const processVideo = async video => {
+  return await copyVideo(video)
 }
